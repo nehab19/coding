c27b23e7d239ed881381d6c486e38ef67bfa7616
Neha Budhiraja <35890155+nehab19@users.noreply.github.com>
2024-11-10 23:19:22 +0530

    Delete streams directory

--- streams/SumNaturalNumber.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.spring5webapp.streams;
-
-import java.util.stream.IntStream;
-
-public class SumNaturalNumber {
-    public static void main(String[] args) {
-        System.out.println(IntStream.rangeClosed(1,10).sum());
-    }
-}
